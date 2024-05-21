e2ed5b987294f84b35107e6fdc0797bd9db77152
hanarahmati <hana.rahmati@gmail.com>
2024-05-21 09:01:26 -0400

    feat: dialog component with logout concept

--- app/src/main/java/co/fanavari/mycomposeapplication/screens/ProfileScreen.kt
+++ app/src/main/java/co/fanavari/mycomposeapplication/screens/ProfileScreen.kt
@@ -3,14 +3,21 @@ package co.fanavari.mycomposeapplication.screens
 import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import co.fanavari.mycomposeapplication.screens.components.FilterChipExample
@@ -41,6 +48,9 @@ fun ProfileScreen(
             TODO("VERSION.SDK_INT < O")
         }
     }
+
+    var showDialog by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -60,5 +70,42 @@ fun ProfileScreen(
         profileScreenViewModel.filteredItems.value.forEach { item ->
             Text(text = item)
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {showDialog = true}) {
+            Text(text = "Show Dialog")
+            
+        }
     }
+    if (showDialog) {
+        ConfirmationDialog(
+            onConfirm = {
+                showDialog = false
+                navController.navigate("login")
+            },
+            onDismiss = { showDialog = false }
+        )
+    }
+}
+
+@Composable
+fun ConfirmationDialog(onConfirm: () -> Unit, onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = "Confirm Action")
+        },
+        text = {
+            Text("Are you sure you want to log out?")
+        },
+        confirmButton = {
+            Button(onClick = onConfirm) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Dismiss")
+            }
+        }
+    )
 }
